0135dd0382c50d459f435ca147c08cb899a47479
Silasico <idowusilas2019@gmail.com>
2023-09-20 22:39:54 +0100

    Building Create Event Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,15 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import Constants from "expo-constants"
 import TopBarNavigator from "../navigators/TopBarNavigation"
+import TextOpen from "../components/TextOpen"
 
 const Home = () => {
   return (
     <View style = { styles.container}>
-      <Text style = {styles.header}>Timeline</Text>
+      <TextOpen 
+        style = {styles.header}
+        font = {"OpenSans_700Bold"}
+      >Timeline</TextOpen>
       
       <TopBarNavigator />
     </View>
