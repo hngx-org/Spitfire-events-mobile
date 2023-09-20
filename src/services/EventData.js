5d52553802fe2441ad452af8dd0f82e922439931
AKintunde Owojori <owojoriakintunde@gmail.com>
2023-09-20 09:50:23 +0100

    Added Calendar

---
+++ src/services/EventData.js
@@ -0,0 +1,21 @@
+const eventData = [
+  {
+    id: 1,
+    title: "Movie Night",
+    address: "Genesis cinemas, Festac",
+    startTime: "10:00 AM",
+    endTime: "12:00 PM",
+    date: "2023-09-20", // Use ISO date format (YYYY-MM-DD)
+  },
+  {
+    id: 2,
+    title: "Event 23",
+    address: "Elm St, Town, Country",
+    startTime: "2:00 PM",
+    endTime: "4:00 PM",
+    date: "2023-09-22",
+  },
+  // Add more events as needed
+];
+
+export default eventData;
