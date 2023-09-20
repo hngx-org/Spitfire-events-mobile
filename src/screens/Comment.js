9d5602293f1ae5691b733d533bfefe5d55b9014f
unknown <smartbizlord@gmail.com>
2023-09-20 12:03:01 +0100

    added some style to the top date component and styled the view even card to have a blue backgroundbefore I will change it to its normal color

--- src/screens/Comment.js
+++ src/screens/Comment.js
@@ -1,48 +1,71 @@
-import { StyleSheet,TouchableWithoutFeedback, ScrollView, Keyboard, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet,TouchableWithoutFeedback, ScrollView, Keyboard, Text, View, Image, TouchableOpacity, Dimensions, } from 'react-native'
 import React from 'react'
 import colors from '../layouts/colors'
+
+
+
+const { height, width } = Dimensions.get("window")
 
 
 const Comment = () => {
   const commentsArr = ["a", "a", "a", "a",]
   return (
-    <TouchableWithoutFeedback>
-      <View>
-        {/* Event Details */}
-        <View>
-          <View>
-            <View></View>
-            <View></View>
+    <SafeAreaView>
+      <TouchableWithoutFeedback>
+        <View style={styles.screenWrapper}>
+          {/* Event Details */}
+          <Text style={styles.dater}>Today</Text>
+          <View style={styles.eventDetailsWrapper}>
+            <View>
+              <View></View>
+              <View></View>
+            </View>
+            <View>
+              <View></View>
+              <View></View>
+            </View>
           </View>
-          <View>
-            <View></View>
-            <View></View>
-          </View>
+          {/* Comments */}
+          <ScrollView>
+            {commentsArr.map((item, index) => {
+              return (
+                <View key={index}><Text>Hello</Text></View>
+              )
+            })}
+          </ScrollView>
+        {/* Input */}
+        <View style={styles.inputWrapper}></View>
         </View>
-        {/* Comments */}
-        <ScrollView>
-          {commentsArr.map((item, index) => {
-            return (
-              <View key={index}><Text>Hello</Text></View>
-            )
-          })}
-        </ScrollView>
-      {/* Input */}
-      <View style={styles.inputWrapper}></View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   )
 }
 
 export default Comment
 
 const styles = StyleSheet.create({
+  screenWrapper: {
+    alignItems: "center",
+    width: width,
+    height: height,
+  },
   eventDetailsWrapper: {
     borderRadius: 16,
-    width: "94%",
-    height: 400,
+    width: "85%",
+    height: 250,
+    backgroundColor: "blue"
   },
   inputWrapper: {
     position: "absolute",
-  }
+  },
+  dater: {
+    backgroundColor: "yellow",
+    color: "orange",
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    borderRadius: 16,
+    marginBottom: 15,
+    marginTop: 5
+  },
+  sdater: {},
 })
