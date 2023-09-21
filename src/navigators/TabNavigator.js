2d3af722077e851e8ba2b51f14b494eabc57e957
AKintunde Owojori <owojoriakintunde@gmail.com>
2023-09-21 06:41:57 +0100

    Created People Stack

--- src/navigators/TabNavigator.js
+++ src/navigators/TabNavigator.js
@@ -1,8 +1,8 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import HomeStack from "./HomeStack";
 import Settings from "../screens/Settings";
-import People from "../screens/People";
 import CalendarScreen from "../screens/Calendar";
+import PeopleStack from "./PeopleStack";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,7 +10,7 @@ const TabNavigator = () => {
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
       <Tab.Screen name="Timeline" component={HomeStack} />
-      <Tab.Screen name="People" component={People} />
+      <Tab.Screen name="People" component={PeopleStack} />
       <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
