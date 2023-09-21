1da2e888584988100ebbafe855ed12159c12c265
unknown <smartbizlord@gmail.com>
2023-09-21 17:44:12 +0100

    worked on the authentication but have not tested because of low battery

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,40 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import React, {createContext, useEffect, useState} from 'react';
+import * as webBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {BASE_URL} from '../config';
+
+
+webBrowser.maybeCompleteAuthSession();
+
+
+
+const handleSendIDToken = async () => {
+  const user = await AsyncStorage.getItem("@user");
+  if (!user) {
+    if (response?.type === "success") {
+      await getUserInfo(response.authentication.id_token);
+    }
+  } else {
+    setUserInfo(JSON.parse(user));
+  }
+}
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
   const [userInfo, setUserInfo] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+
+  // handling google auth
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "176112084291-j6rruls1vpmnhnfsjctpe555tgm9o9p7.apps.googleusercontent.com",
+    iosClientId:
+      "176112084291-3i88bccbt5jp8urq0vgu50nudoath8k2.apps.googleusercontent.com",
+  });
 
   const register = (name, email, password) => {
     setIsLoading(true);
@@ -31,25 +58,32 @@ export const AuthProvider = ({children}) => {
       });
   };
 
-  const login = (email, password) => {
+  const login = async() => {
     setIsLoading(true);
 
-    axios
-      .post(`${BASE_URL}/login`, {
-        email,
-        password,
-      })
-      .then(res => {
-        let userInfo = res.data;
-        console.log(userInfo);
-        setUserInfo(userInfo);
-        AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
-        setIsLoading(false);
-      })
-      .catch(e => {
-        alert(`login error ${e}`);
-        setIsLoading(false);
-      });
+    promptAsync()
+    if(response) {
+      if (response?.type === "success") {
+        const token = response.authentication.id_token
+        axios
+          .post(`${BASE_URL}/auth`, {
+            token,
+          })
+          .then(res => {
+            let userInfo = res.data;
+            console.log(userInfo);
+            setUserInfo(userInfo);
+            AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
+            setIsLoading(false);
+          })
+          .catch(e => {
+            alert(`login error ${e}`);
+            setIsLoading(false);
+          });
+      }
+    }
+
+    
   };
 
   const logout = () => {
