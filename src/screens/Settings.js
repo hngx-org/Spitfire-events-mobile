637056ee333bf0b013ad360946d49a872b626b93
Holutunde <Holutunde99@gmail.com>
2023-09-21 14:07:39 +0100

    started settings

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 
 const Settings = () => {
