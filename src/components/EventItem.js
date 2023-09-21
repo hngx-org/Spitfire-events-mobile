1609706d74deccacda7ae391f20042971a5194c1
AKintunde Owojori <owojoriakintunde@gmail.com>
2023-09-21 18:56:14 +0100

    NOTHING MUCH

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -1,6 +1,6 @@
 import {View, Text, Image, StyleSheet} from "react-native";
-import colors from "../layouts/colors"
-import openSans from "../layouts/fonts"
+import colors from "../layouts/colors";
+import openSans from "../layouts/fonts";
 import TextOpen from "./TextOpen";
 
 const EventItem = () => {
@@ -12,61 +12,57 @@ const EventItem = () => {
           style={{width: 50, height: 50}}
         />
       </View>
-      <View style = {{gap: 5, flex: 1}}>
-        <TextOpen 
-          style = {styles.title}
-          font = {"OpenSans_700Bold"}
-        >Football Game</TextOpen>
-        <TextOpen 
-          style = {styles.date}
-          font = {"OpenSans_600SemiBold"}
-        >May 20, 2023</TextOpen>
-        <TextOpen 
-          style = {styles.time}
-          font = {"OpenSans_600SemiBold"}
-        >Friday 4-6pm</TextOpen>
-        <TextOpen 
-          style = {styles.location}
-          font = {"OpenSans_700Bold"}
-        >Teslim Balogun Stadium</TextOpen>
-      <View style={{gap: 2, flex: 1}}>
-        <Text style={styles.title}>Football Game</Text>
-        <Text style={styles.date}>May 20, 2023</Text>
-        <Text style={styles.time}>Friday 4-6pm</Text>
-        <Text style={styles.location}>Teslim Balogun Stadium</Text>
-      </View>
+      <View style={{gap: 5, flex: 1}}>
+        <TextOpen style={styles.title} font={"OpenSans_700Bold"}>
+          Football Game
+        </TextOpen>
+        <TextOpen style={styles.date} font={"OpenSans_600SemiBold"}>
+          May 20, 2023
+        </TextOpen>
+        <TextOpen style={styles.time} font={"OpenSans_600SemiBold"}>
+          Friday 4-6pm
+        </TextOpen>
+        <TextOpen style={styles.location} font={"OpenSans_700Bold"}>
+          Teslim Balogun Stadium
+        </TextOpen>
+        <View style={{gap: 2, flex: 1}}>
+          <Text style={styles.title}>Football Game</Text>
+          <Text style={styles.date}>May 20, 2023</Text>
+          <Text style={styles.time}>Friday 4-6pm</Text>
+          <Text style={styles.location}>Teslim Balogun Stadium</Text>
+        </View>
 
-      <View
-        style={{
-          justifyContent: "space-between",
-          gap: 70,
-          alignItems: "flex-end",
-        }}
-      >
-        <Image
-          source={require("../../assets/icons/menu-icon.png")}
-          style={{width: 20, height: 20}}
-        />
-        <View style={{flexDirection: "row", gap: 5, alignItems: "center"}}>
-          <View
-            style={{
-              padding: 3,
-              backgroundColor: "rgba(0, 200, 0, 1)",
-              borderRadius: 3,
-            }}
-          ></View>
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "bold",
-              color: "rgba(124, 20, 155, 1)",
-            }}
-          >
-            LIVE
-          </Text>
+        <View
+          style={{
+            justifyContent: "space-between",
+            gap: 70,
+            alignItems: "flex-end",
+          }}
+        >
+          <Image
+            source={require("../../assets/icons/menu-icon.png")}
+            style={{width: 20, height: 20}}
+          />
+          <View style={{flexDirection: "row", gap: 5, alignItems: "center"}}>
+            <View
+              style={{
+                padding: 3,
+                backgroundColor: "rgba(0, 200, 0, 1)",
+                borderRadius: 3,
+              }}
+            ></View>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "bold",
+                color: "rgba(124, 20, 155, 1)",
+              }}
+            >
+              LIVE
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
     </View>
   );
 };
--- src/screens/People.js
+++ src/screens/People.js
@@ -1,52 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList, Platform, StatusBar } from 'react-native';
-import ImageCard from '../components/People/ImageCard';
-import TextOpen from '../components/TextOpen';
-import AppButton from '../components/AppButton';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Platform,
+  StatusBar,
+} from "react-native";
+import ImageCard from "../components/People/ImageCard";
+import TextOpen from "../components/TextOpen";
+import AppButton from "../components/AppButton";
 
 const People = () => {
-
   const data = [
     {
-      name: 'Ybnl mafia',
-      img: require('../../assets/images/ybnlmafia.png')
+      name: "Ybnl mafia",
+      img: require("../../assets/images/ybnlmafia.png"),
     },
 
     {
-      name: 'Techies',
-      img: require('../../assets/images/techies.png')
-    }
-  ]
+      name: "Techies",
+      img: require("../../assets/images/techies.png"),
+    },
+  ];
 
   return (
     <View style={styles.container}>
-      <TextOpen 
-        style = {styles.header}
-        font = {"OpenSans_700Bold"}
-      >My People</TextOpen>
+      <TextOpen style={styles.header} font={"OpenSans_700Bold"}>
+        My People
+      </TextOpen>
       <FlatList
         data={data}
-        renderItem={({item}) => <ImageCard data={item}/>}
-        ItemSeparatorComponent={()=> <View style={{height: 20}}/>}
+        renderItem={({item}) => <ImageCard data={item} />}
+        ItemSeparatorComponent={() => <View style={{height: 20}} />}
       />
 
       <View style={styles.buttonContainer}>
-        <AppButton title={'Add'}/>
+        <AppButton title={"Add"} />
       </View>
-      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
     paddingHorizontal: 20,
     gap: 20,
-    display: 'flex',
-
+    display: "flex",
   },
-  
+
   header: {
     fontWeight: "bold",
     color: "rgba(124, 20, 155, 1)",
@@ -54,8 +57,8 @@ const styles = StyleSheet.create({
   },
 
   buttonContainer: {
-    marginTop: 25
-  }
+    marginTop: 25,
+  },
 });
 
 export default People;
