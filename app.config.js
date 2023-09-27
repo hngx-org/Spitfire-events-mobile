009292e0ccfb7381d7a1199f6a1b53eeaa6bfedf
AKintunde Owojori <owojoriakintunde@gmail.com>
2023-09-27 22:02:34 +0100

    app config

--- app.config.js
+++ app.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  name: 'spitfire',
-  version: '1.0.0',
+  name: "spitfire-apk",
+  version: "1.0.0",
   extra: {
     clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+  },
+  expo: {
+    android: {
+      package: "com.spitfire.event",
+    },
+    extra: {
+      eas: {
+        projectId: "dede9e52-2166-41ef-bf26-6878164946b7",
+      },
+    },
   },
 };
